5f3dcab0cddbfc8597723201ed7c782bf034b8ad
ianfajardo <ianfajardozx@gmail.com>
2021-01-28 15:11:34 -0500

    seperate inputs & add listeners

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 //calculating script
 
+      $("#a1").keyup(function() {
+        var a1 = parseInt($("#a1").val());
+        $(".a-input").val(a1);
+      });
+
+      $("#p1").keyup(function() {
+        var p1 = parseInt($("#p1").val());
+        $(".p-input").val(p1);
+      });
+
       //on calculate click
       $("#form1").click(function () {
         //get values from form one
@@ -24,14 +34,14 @@
       $("#form2").click(function () {
         //get values from form one
         var b2 = parseInt($("#b2").val());
-        var a2 = parseInt($("#a2").val());
-        var p2 = parseInt($("#p2").val());
+        var a1 = parseInt($("#a1").val());
+        var p1 = parseInt($("#p1").val());
         if (
           Number.isInteger(b2) &&
-          Number.isInteger(a2) &&
-          Number.isInteger(p2)
+          Number.isInteger(a1) &&
+          Number.isInteger(p1)
         ) {
-          var val1 = Math.pow(b2, a2) % p2;
+          var val1 = Math.pow(b2, a1) % p1;
           console.log(val1);
           $("#S2").val(val1);
         }
